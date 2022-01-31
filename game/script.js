9e94efd0e7d9615b38c41934d0d3a628fb2d7de7
Youssef-14 <97913657+Youssef-14@users.noreply.github.com>
2022-01-31 21:55:36 +0100

    Update script.js

--- game/script.js
+++ game/script.js
@@ -572,14 +572,12 @@ $(function() {
      //restart game
     $('#'+ game.id +' .restart').on('click', function(){
         game.init().run();
-        var audio = new Audio('son.mp3');
         audio.play();
     });
   
     $('#sudoku_menu .restart').on('click', function(){
         game.init().run();
         $('#sudoku_menu').removeClass('open-sidebar');
-                var audio = new Audio('son.mp3');
         audio.play();
     });
     
